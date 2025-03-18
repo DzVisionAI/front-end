0df3bdcd2e66800548db400d8618cfe99b282bad
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2025-03-18 17:21:47 +0100

    ⚡init⚡

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Architects_Daughter, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap'
+})
+
+const architects_daughter = Architects_Daughter({
+  subsets: ['latin'],
+  variable: '--font-architects-daughter',
+  weight: '400',
+  display: 'swap'
+})
+
+export const metadata: Metadata = {
+  title: "DzVisionAI",
+  description: "AI-Powered Military Surveillance System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning
+        className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}
+      >
+        <div className="flex flex-col min-h-screen overflow-hidden">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
