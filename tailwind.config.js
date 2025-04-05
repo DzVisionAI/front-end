1920712b0afcd50270e0f69fdbe887a5c48e4bff
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2025-04-05 03:12:07 +0100

    📄 Design Dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        './src/**/*.{js,ts,jsx,tsx}',
         './app/**/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
@@ -71,4 +72,5 @@ module.exports = {
             },
         },
     },
+    plugins: [],
 }
